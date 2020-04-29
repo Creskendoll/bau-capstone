325004886e84353d36a503bc862ca5c09e5fa02c
JohannesJoestar <thejohannesjoestar@gmail.com>
2020-04-29 11:54:46 +0300

    missing shuffle function

--- src/misc/Helpers.ts
+++ src/misc/Helpers.ts
@@ -6,4 +6,16 @@ const sleep = (ms: number) => {
 
 const pickRand = (arr: any[]) => arr[Math.floor(Math.random() * arr.length)];
 
+// The Fisher-Yates shuffle
+export const shuffle = (array: any[]) => {
+  for (let current = array.length - 1; current > 0; current--) {
+    let random = Math.floor(Math.random() * (current + 1));
+
+    let swap = array[current]; 
+    array[current] = array[random]; 
+    array[random] = swap
+  }
+  return array;
+};
+
 export { sleep, pickRand };
